fca599f7cce814b2108fce7251e45cccabdb0024
souperwit <sirawit.mn@gmail.com>
2021-03-20 00:30:05 +0700

    changed script link to /JSX
    add css (inline +external)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+const myName = "Sirawit Mahanin"
+const currentYear = new Date().getFullYear();
+const myImg = "https://picsum.photos/200"
 
-const name = "Sirawit"
-const surname = "Mahanin"
-const luckyNumber = 7
-
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+}
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name} {surname}!</h1>
-    <p>My lucky number is {luckyNumber}.</p>
+    <h1 className="heading" contentEditable="true" spellCheck="false">Created by {myName}.</h1>
+    <p>Copyright {currentYear}.</p>
+    <img src={myImg+"?grayscale"} alt='raiwa'/>
+
+    {/*Style ต้องการ Value ที่เป็น JS Object เลขต้องทำให้เป็น Object ยังไงล่ะ*/}
+    <p style= {customStyle}>Test inline style yr: {currentYear}.</p>
   </div>,
   document.getElementById('root')
 );
-
