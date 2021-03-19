d0a30b29d4fbca5686fbf637083286093206d832
souperwit <sirawit.mn@gmail.com>
2021-03-19 22:45:01 +0700

    Insert simple js expression

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const name = "Sirawit"
+const surname = "Mahanin"
+const luckyNumber = 7
+
+
 ReactDOM.render(
   <div>
-    <h1>Hello, World!</h1>
-    <p>This is paragraph!</p>
+    <h1>Hello {name} {surname}!</h1>
+    <p>My lucky number is {luckyNumber}.</p>
   </div>,
   document.getElementById('root')
 );
