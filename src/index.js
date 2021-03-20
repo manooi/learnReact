8eae3282d0ca495c8d4ffa98ed6de01b17ab6301
souperwit <sirawit.mn@gmail.com>
2021-03-21 01:08:49 +0700

    comlicated time compare

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
 import './index.css';
+
+
 const myName = "Sirawit Mahanin"
 const currentYear = new Date().getFullYear();
 const myImg = "https://picsum.photos/200"
@@ -17,6 +20,38 @@ if ((1+1)=== 2) {
 };
 
 
+const isTimeBetween = function(startTime, endTime, serverTime) {
+    let start = moment(startTime, "H:mm")
+    let end = moment(endTime, "H:mm")
+    let server = moment(serverTime, "H:mm")
+    if (end < start) {
+        return server >= start && server<= moment('23:59:59', "h:mm:ss") || server>= moment('0:00:00', "h:mm:ss") && server < end;
+    }
+    return server>= start && server< end
+}
+
+
+
+var word = 'Good ';
+// var testDate = new Date();
+console.log(testDate)
+var testDate = new Date('March 21, 2021 19:00:00');
+
+
+const customColor = {}
+
+if (isTimeBetween('00:00', '12:00', testDate )) {
+  word += "Morning";
+  customColor.color = "red";
+} else if (isTimeBetween('12:00', '18:00', testDate )) {
+  word += "Afternoon";
+  customColor.color = "green";
+} else {
+  word += "Night ";
+  customColor.color = "blue";
+}
+
+
 ReactDOM.render(
   <div>
     <h1 className="heading" contentEditable="true" spellCheck="false">Created by {myName}.</h1>
@@ -25,6 +60,9 @@ ReactDOM.render(
 
     {/*Style ต้องการ Value ที่เป็น JS Object เลขต้องทำให้เป็น Object ยังไงล่ะ*/}
     <p style= {customStyle}>Test inline style yr: {currentYear}.</p>
+
+    <h1 style= {customColor} className="heading">{word}</h1>
+
   </div>,
   document.getElementById('root')
 );
