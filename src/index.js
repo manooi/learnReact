4dc3229588bfd19cb2ad8fc87f7e468770b0a43d
souperwit <sirawit.mn@gmail.com>
2021-03-21 01:17:58 +0700

    simplified

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import moment from 'moment';
 import './index.css';
 
-
 const myName = "Sirawit Mahanin"
 const currentYear = new Date().getFullYear();
 const myImg = "https://picsum.photos/200"
@@ -14,43 +13,22 @@ const customStyle = {
   border: "1px solid black"
 };
 
-if ((1+1)=== 2) {
-  customStyle.color ="green";
-  customStyle.textAlign="center";
-};
-
-
-const isTimeBetween = function(startTime, endTime, serverTime) {
-    let start = moment(startTime, "H:mm")
-    let end = moment(endTime, "H:mm")
-    let server = moment(serverTime, "H:mm")
-    if (end < start) {
-        return server >= start && server<= moment('23:59:59', "h:mm:ss") || server>= moment('0:00:00', "h:mm:ss") && server < end;
-    }
-    return server>= start && server< end
-}
-
-
-
-var word = 'Good ';
-// var testDate = new Date();
-console.log(testDate)
-var testDate = new Date('March 21, 2021 19:00:00');
-
-
+let word = 'Good ';
 const customColor = {}
+// var testDate = new Date();
+const currentDate = new Date('March 21, 2021 18:00:00');
+const currentTime = currentDate.getHours()
 
-if (isTimeBetween('00:00', '12:00', testDate )) {
+if (currentTime < 12) {
   word += "Morning";
   customColor.color = "red";
-} else if (isTimeBetween('12:00', '18:00', testDate )) {
+} else if (currentTime < 18) {
   word += "Afternoon";
   customColor.color = "green";
 } else {
   word += "Night ";
   customColor.color = "blue";
 }
-
 
 ReactDOM.render(
   <div>
