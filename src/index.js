b3c4db5a3216262304ebdccf9dc6fa0be3fee65c
souperwit <sirawit.mn@gmail.com>
2021-03-21 21:16:44 +0700

    Add App.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Heading from "./Heading.jsx"; //ES6 no need to put extension
-import List from "./List.jsx";
-import './index.css';
+import App from './App.js'
 
 ReactDOM.render(
-  <div>
-    <Heading />
-    <List />
-  </div>,
+  <App />,
   document.getElementById("root")
-)
+);
 
 
